9c2821067040c2e0e0b0634f511274b68ee09a4f
Koli-pal <palkoli581@gmail.com>
2025-02-02 22:36:56 +0600

    commit now

---
+++ src/test/java/com/automation/utilities/ExcelUtils.java
@@ -0,0 +1,13 @@
+package com.automation.utilities;
+
+import java.io.IOException;
+
+public class ExalUtils {
+    public static String username;
+    public static String password;
+    @SuppressWarnings({"resource","unused"})
+    public void ReadExcel()throws IOException{
+        String excelFilePath = "./TestData/Automation test data 1.xlsx";
+
+    }
+}
